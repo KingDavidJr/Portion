60f66ae7a23f9abf7dce7aa77d2ca5d3831f1dd6
David <realkd872@gmail.com>
2025-03-02 11:31:18 -0600

    Initial Commit

---
+++ Portion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Portion
+//
+//  Created by David Amedeka on 3/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Portion/PortionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortionApp.swift
+//  Portion
+//
+//  Created by David Amedeka on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PortionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
