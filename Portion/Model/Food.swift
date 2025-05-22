aed8a82769d2eae74a14fdca67ad0c6ecd884281
David <realkd872@gmail.com>
2025-05-22 09:55:20 -0500

    Moved to Model Folder

---
+++ Portion/Model/Food.swift
@@ -0,0 +1,27 @@
+//
+//  FoodModel.swift
+//  Portion
+//
+//  Created by David Amedeka on 3/2/25.
+//
+
+struct Food: Codable {
+    let image: String
+    let title: String
+    let servings: Int
+    let extendedIngredients: [Ingredients]
+    
+    init(image: String = "", title: String, servings: Int, extendedIngredients: [Ingredients]) {
+        self.title = title
+        self.servings = servings
+        self.extendedIngredients = extendedIngredients
+        self.image = image
+    }
+    
+    init() {
+        self.title = ""
+        self.servings = 0
+        self.extendedIngredients = []
+        self.image = ""
+    }
+}
