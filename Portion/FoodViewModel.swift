fe542e3c0eef2ab12306c91937e8e48716d580e6
David <realkd872@gmail.com>
2025-05-23 06:00:43 -0500

    Completed the fetchFoodsFromRecipe function:
        Added a check for if the input url is empty
        Added a check for if the input url is not valid for spoonacular
    
        Created the URL Request (The secret is hardcoded, but this is intended for testing purposes. The secret should changed for production, and should access it from the server or passkeys).
    
        Handeled the fetch task using the created URL Request, which decodes the returned JSON to a Food Class.
    
        Is Loading state was also created and managed in this viewModel

--- Portion/FoodViewModel.swift
+++ Portion/FoodViewModel.swift
@@ -9,5 +9,49 @@ import Foundation
 @Observable
 @MainActor // Ensure changes to UI runs on main thread
 final class FoodViewModel {
+    var isLoading: Bool = false
+    var food: Food?
     
+    func fetchFoodsFromRecipe(from recipeURL: String) async {
+        // Guard clauses to ensure a Valid URL
+        guard !recipeURL.isEmpty else {
+            //MARK: Handle error
+            return
+        }
+        
+        guard let url = URL(string: "https://api.spoonacular.com/recipes/extract?url=\(recipeURL)") else {
+            //MARK: Handle error
+            return
+        }
+        
+        // Create URL Request
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("38b3cb2c7f4a4f87be615bc96c566877", forHTTPHeaderField: "x-api-key")
+        
+        // Handle URL Fetch Task
+        do {
+            self.isLoading = true // Start Loading State while code is running
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            if let httpResponse = response as? HTTPURLResponse,
+               httpResponse.statusCode == 200 {
+                do {
+                    let decodedRecipe = try JSONDecoder().decode(Food.self, from: data)
+                    self.food = decodedRecipe
+                    self.isLoading = false
+                } catch {
+                    //MARK: Handle this error
+                    print(error.localizedDescription)
+                    isLoading = false
+                }
+            }
+        } catch {
+            //MARK: Handle this error
+            print(error.localizedDescription)
+            isLoading = false
+        }
+    }
 }
