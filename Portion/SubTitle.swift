3cf69cfbe4ad82d5b5954cd75d8a68736f2dae93
David <realkd872@gmail.com>
2025-06-14 09:28:26 -0500

    Init file

---
+++ Portion/SubTitle.swift
@@ -0,0 +1,8 @@
+//
+//  SubTitle.swift
+//  Portion
+//
+//  Created by David Amedeka on 6/14/25.
+//
+
+import Foundation
---
+++ Portion/UniversalButton.swift
@@ -0,0 +1,18 @@
+//
+//  UniversalButton.swift
+//  Portion
+//
+//  Created by David Amedeka on 6/14/25.
+//
+
+import SwiftUI
+
+struct UniversalButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UniversalButton()
+}
