2cbfdb11bd05239b63fe89d8f49d56dcd1aee03b
David <realkd872@gmail.com>
2025-06-14 09:29:24 -0500

    Added variables to specify the color of the button, and the text for the button.
    Added inits for the button for flexibility.

--- Portion/UniversalButton.swift
+++ Portion/UniversalButton.swift
@@ -8,6 +8,18 @@
 import SwiftUI
 
 struct UniversalButton: View {
+    var color: Color
+    var text: String
+    
+    init(color: Color, text: String) {
+        self.color = color
+        self.text = text
+    }
+    
+    init() {
+        self.color = .blue
+        self.text = ""
+    }
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
