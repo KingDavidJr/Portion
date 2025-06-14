3bc42558f1c4def9d963aea5ed796fb27f1ebece
David <realkd872@gmail.com>
2025-06-14 10:00:28 -0500

    init file

---
+++ Portion/SubTitleSupportText.swift
@@ -0,0 +1,15 @@
+//
+//  SubTitleSupportText.swift
+//  Portion
+//
+//  Created by David Amedeka on 6/14/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct SubTitleSupportText : ViewModifier {
+    func body(content: Content) -> some View {
+        
+    }
+}
