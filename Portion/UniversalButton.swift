ef26e07c7c1e226ff2162a14e17ac5dee00a6ee6
David <realkd872@gmail.com>
2025-06-14 09:34:06 -0500

    Added capability to customize textColor

--- Portion/UniversalButton.swift
+++ Portion/UniversalButton.swift
@@ -10,15 +10,18 @@ import SwiftUI
 struct UniversalButton: View {
     var color: Color
     var text: String
+    var textColor: Color
     
-    init(color: Color, text: String) {
+    init(color: Color, text: String, textColor: Color) {
         self.color = color
         self.text = text
+        self.textColor = textColor
     }
     
     init() {
         self.color = .blue
         self.text = ""
+        self.textColor = .white
     }
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
